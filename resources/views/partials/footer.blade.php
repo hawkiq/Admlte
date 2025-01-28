eaf5f5ca2d3830cd392acc1487928780437ec976
hawkiq <osama.sw87@yahoo.com>
2025-01-28 08:50:46 +0300

    Fixed footer section

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,10 +1,7 @@
 <!--begin::Footer-->
 <footer class="app-footer">
     <!--begin::To the end-->
-    <div class="float-end d-none d-sm-inline">@yield('footer_text')</div>
+    @yield('footer')
     <!--end::To the end-->
-    <!--begin::Copyright-->
-    @yield('copyright')
-    <!--end::Copyright-->
 </footer>
 <!--end::Footer-->
